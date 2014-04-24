ad0afda1f2746780f4c012ffd537aa1652c1349f
Emilien BAUDET <ebaudet@e1r4p14.42.fr>
2014-04-24 20:03:39 +0200

    avancement

--- Makefile
+++ Makefile
@@ -6,26 +6,26 @@
 #    By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2014/04/23 12:31:26 by ebaudet           #+#    #+#              #
-#    Updated: 2014/04/23 13:27:14 by ebaudet          ###   ########.fr        #
+#    Updated: 2014/04/24 19:54:31 by ebaudet          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME		= nm_otool
 
 NM			= ft_nm
-F_NM		= nm.c
+F_NM		= nm.c ft_pow.c ft_puthex.c
 SRC_NM		= $(addprefix srcs/, $(F_NM))
 OBJ_NM		= $(SRC_NM:srcs/%.c=.obj/%.o)
 
 OTOOL		= ft_otool
-F_OTOOL		= otool.c
+F_OTOOL		= otool.c ft_pow.c ft_puthex.c
 SRC_OTOOL	= $(addprefix srcs/, $(F_OTOOL))
 OBJ_OTOOL	= $(SRC_OTOOL:srcs/%.c=.obj/%.o)
 
 INC			= -I includes -I libft/includes
 FLAGS		= -Wall -Wextra -Werror
 LIB			= -L libft -lft
-CC			= gcc
+CC			= cc
 
 all: $(NAME)
 
@@ -53,4 +53,8 @@ fclean: clean
 
 re: fclean all
 
-.PHONY: all clean fclean re
+gdb: make_libft
+	cc -g $(FLAGS) $(SRC_OTOOL) -o $(OTOOL) $(INC) $(LIB)
+	cc -g $(FLAGS) $(SRC_NM) -o $(NM) $(INC) $(LIB)
+
+.PHONY: all clean fclean re gdb
--- srcs/nm.c
+++ srcs/nm.c
@@ -6,25 +6,11 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/23 12:32:36 by ebaudet           #+#    #+#             */
-/*   Updated: 2014/04/23 23:40:14 by ebaudet          ###   ########.fr       */
+/*   Updated: 2014/04/24 19:53:52 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-
-int		main(int ac, char **av)
-{
-	(void)av;
-	char	*file;
-
-	if (ac == 1)
-	{
-		file = ft_strdup("a.out");
-		ft_putstr(file);
-	}
-}*/
-
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,9 +18,12 @@ int		main(int ac, char **av)
 #include <mach-o/loader.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include "math.h"
 
-int main(int argc, char *argv[]) {
-	(void)argc;
+
+
+void	ft_otool(char *av)
+{
 	int fd;
 	struct stat stat_buf;
 	size_t size;
@@ -44,15 +33,16 @@ int main(int argc, char *argv[]) {
 	struct load_command *lc;
 	struct segment_command_64 *sc;
 	struct section_64 *section;
+	char	*copy;
 
 	// Open the file and get its size
-	fd = open(argv[1], O_RDONLY);
+	fd = open(av, O_RDONLY);
 	fstat(fd, &stat_buf);
 	size = stat_buf.st_size;
 
 	// Map the file to memory
 	addr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_FILE | MAP_PRIVATE, fd, 0);
-
+	copy = addr;
 	// The first bytes of a Mach-O file comprise its header
 	mh = (struct mach_header_64 *)addr;
 
@@ -67,133 +57,69 @@ int main(int argc, char *argv[]) {
 	while (i < mh->ncmds)
 	{
 		lc = (struct load_command *)addr;
-
-		ft_putnbr(i);
-		ft_putchar('\n');
+		
 		if (lc->cmdsize == 0)
+		{
+			i++;
 			continue;
-
-		// If the load command is a (64-bit) segment,
-		// print  about the segment
+		}
 		if (lc->cmd == LC_SEGMENT_64)
 		{
 			sc = (struct segment_command_64 *)addr;
-
-			ft_putstr("test0");
 			if (ft_strcmp(sc->segname, "__TEXT") == 0)
 			{
-				unsigned int		i;
+				unsigned int		k;
 
-				i = 0;
+				k = 0;
 				addr += sizeof(struct segment_command_64);
 				section = (struct section_64 *)addr;
-				while (i < sc->nsects)
+				while (k < sc->nsects)
 				{
 					section = (struct section_64 *)addr;
 					addr += sizeof(struct section_64);
 					///section += section->offset;
-					ft_putstr("sectname : ");
-					ft_putstr(section->sectname);
-					ft_putstr("\n");
 					if (ft_strcmp(section->sectname, "__text") == 0)
 					{
-						ft_putstr("coucou");
-						char	*val = (char *)section->addr;
-						// unsigned int	i = 0;
-						ft_putendl("texte :");
-						printf("%p\n%llu\n", val, section->size);
+						unsigned int		j;
 
-						printf("%s\n", (char *)section->addr);
-						return (0);
-						/*while (i < section->size)
+						j = 0;
+						while (j < section->size)
 						{
-							ft_putchar(val[i]);
-							i++;
-						}*/
-						/*printf("\nSECTION_64:\nsectname %s\n\t"
-							"segname %s\n\t"
-							"addr 0x%llu\n\t"
-							"size 0x%llu\n\t"
-							"offset 0x%u\n\t"
-							"align 0x%u\n\t"
-							"reloff 0x%u\n\t"
-							"nreloc 0x%u\n\t"
-							"flags 0x%u\n\t"
-							"reserved1 0x%u\n\t"
-							"reserved2 0x%u\n\t"
-							"reserved3 0x%u\n\t",
-							section->sectname,
-							section->segname,
-							section->addr,
-							section->size,
-							section->offset,
-							section->align,
-							section->reloff,
-							section->nreloc,
-							section->flags,
-							section->reserved1,
-							section->reserved2,
-							section->reserved3
-						);*/
+							if (!(j % 16))
+							{
+								ft_putchar('\n');
+								ft_puthex(section->addr + j, 16);
+							}
+							ft_putchar(' ');
+							ft_puthex(copy[section->offset + j] & 0xFF, 2);
+								//printf("\n%x ", (unsigned int)section->addr + j);
+							// printf("| %p (%u)- ", copy, section->offset);
+							/*printf("%02x ", copy[section->offset + j] & 0xFF);*/
+							j++;
+						}
 					}
-
-					i++;
+					k++;
 				}
 			}
-			/*
-			printf("if : '0x%08x'\n", lc->cmd);
-			printf("Segment %s\n\t"
-				"vmaddr 0x%llx\n\t"
-				"vmsize 0x%llx\n\t"
-				"fileoff %llu\n\t"
-				"filesize %llu\n\t"
-				"section %u\n\t"
-				"flags 0x%08x\n",
-				sc->segname,
-				sc->vmaddr,
-				sc->vmsize,
-				sc->fileoff,
-				sc->filesize,
-				sc->nsects,
-				sc->flags);
-			section = (struct section_64 *)addr;
-			printf("\nSECTION_64:\nsectname %s\n\t"
-				"segname %s\n\t"
-				"addr 0x%llu\n\t"
-				"size 0x%llu\n\t"
-				"offset 0x%u\n\t"
-				"align 0x%u\n\t"
-				"reloff 0x%u\n\t"
-				"nreloc 0x%u\n\t"
-				"flags 0x%u\n\t"
-				"reserved1 0x%u\n\t"
-				"reserved2 0x%u\n\t"
-				"reserved3 0x%u\n\t",
-				section->sectname,
-				section->segname,
-				section->addr,
-				section->size,
-				section->offset,
-				section->align,
-				section->reloff,
-				section->nreloc,
-				section->flags,
-				section->reserved1,
-				section->reserved2,
-				section->reserved3
-			);
-			*/
-        }
+		}
+		addr += lc->cmdsize;
+		i++;
+	}
+	munmap(addr, size);
+	close(fd);
+}
 
-        // Advance to the next load command    
-        addr += lc->cmdsize;
-        i++;
-    }
+int		main(int ac, char **av)
+{
+	int		i;
 
-    printf("\nDone.\n");
-
-    munmap(addr, size);
-    close(fd);
-
-    return 0;
+	i = 0;
+	if (ac == 1)
+		ft_otool("a.out");
+	else
+	{
+		while (av[++i])
+			ft_otool(av[i]);
+	}
+	return (0);
 }
