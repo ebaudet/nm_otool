38d5daa5e11ec66dce397e178a8b8e65a155906a
Emilien Baudet <emilien.baudet@gmail.com>
2014-04-27 20:58:26 +0200

    norme

---
+++ srcs/ft_error.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/04/27 20:54:13 by ebaudet           #+#    #+#             */
+/*   Updated: 2014/04/27 20:57:15 by ebaudet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+int		return_error(char *message, char *file)
+{
+	ft_putstr_fd(message, 2);
+	ft_putstr_fd(file, 2);
+	ft_putstr_fd(".\n", 2);
+	return (EXIT_FAILURE);
+}
