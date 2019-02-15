c0f076791266d8a22f549263fd42a8a61e0bc0b3
Emilien Baudet <emilien.baudet@gmail.com>
2019-02-15 01:48:03 +0100

    structure

--- includes/nm.h
+++ includes/nm.h
@@ -6,7 +6,7 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/23 12:32:09 by ebaudet           #+#    #+#             */
-/*   Updated: 2019/01/22 20:33:22 by ebaudet          ###   ########.fr       */
+/*   Updated: 2019/02/15 01:19:30 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,13 @@
 int				print_error(char *message);
 int				file_error(char *message, char *file);
 unsigned int	endian_swap(unsigned int x);
+
+typedef struct			s_ftnm
+{
+	unsigned long	offset;
+	char			symbol;
+	char			*table_index;
+	struct s_ftnm	*next;
+}						t_ftnm;
 
 #endif
