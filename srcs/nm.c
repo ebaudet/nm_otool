8ca5bac1ad31e628d1069705ff696d75c8fb542a
Emilien BAUDET <ebaudet@e2r12p7.42.fr>
2019-02-18 21:28:03 +0100

    error message + clean code

--- srcs/nm.c
+++ srcs/nm.c
@@ -6,7 +6,7 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/23 12:32:36 by ebaudet           #+#    #+#             */
-/*   Updated: 2019/02/16 19:42:41 by ebaudet          ###   ########.fr       */
+/*   Updated: 2019/02/18 19:03:05 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <sys/mman.h>
 #include <mach-o/loader.h>
 #include <mach-o/nlist.h>
+#include <mach-o/fat.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <stdlib.h>
@@ -135,6 +136,33 @@ void	print_output(t_symtable **list)
 	}
 }
 
+t_symtable *add_symtable(struct nlist_64 array, struct section_64 *section, char *stringtable, t_symtable **list)
+{
+	t_symtable	*new;
+	char		*offset;
+
+	if (!array.n_value)
+			offset = ft_strdup("                ");
+	else
+		offset = ft_gethex((unsigned long)array.n_value, 16);
+	ft_printf("%34k->%k {in get_symtable: offset[%16s] symbol[%c] table_index[%s]}\n",
+	          offset,
+	          type(section->sectname, array.n_type, array.n_value, array.n_sect),
+	          stringtable+array.n_un.n_strx);
+	new = new_symtable(offset, type(section->sectname, array.n_type,
+		array.n_value, array.n_sect),
+		stringtable+array.n_un.n_strx);
+	if (new == NULL)
+	{
+		ft_printf("   {elememt list creation %31kERROR%k}\n");
+		return (NULL);
+	}
+	ft_printf("   {new: offset[%16s] symbol[%c] table_index[%s]}\n", new->offset, new->symbol, new->table_index);
+	list_add_order_symtable(list, new, compare_tableindex);
+	ft_printf("   {list add %32kOK%k}\n");
+	return (new);
+}
+
 void	get_symtable(struct symtab_command *sym, int nsyms, char *ptr,
                   t_symtable **list)
 {
@@ -143,8 +171,6 @@ void	get_symtable(struct symtab_command *sym, int nsyms, char *ptr,
 	struct nlist_64				*array;
 	struct segment_command_64	*segment;
 	struct section_64			*section;
-	char						*offset;
-	t_symtable					*new;
 
 	array = (void *)ptr + sym->symoff;
 	stringtable = (void *)ptr + sym->stroff;
@@ -154,22 +180,7 @@ void	get_symtable(struct symtab_command *sym, int nsyms, char *ptr,
 	{
 		ft_printf("%33k<syms %d/%d>%k\n", i, nsyms);
 		section = get_section(segment, array[i].n_sect);
-		if (!array[i].n_value)
-			offset = ft_strdup("                ");
-		else
-			offset = ft_gethex((unsigned long)array[i].n_value, 16);
-		ft_printf("%34k->%k {in get_symtable: offset[%16s] symbol[%c] table_index[%s]}\n",
-		          offset,
-		          type(section->sectname, array[i].n_type, array[i].n_value, array[i].n_sect),
-		          stringtable+array[i].n_un.n_strx);
-		new = new_symtable(offset, type(section->sectname, array[i].n_type,
-			array[i].n_value, array[i].n_sect),
-			stringtable+array[i].n_un.n_strx);
-		ft_printf("   {new: offset[%16s] symbol[%c] table_index[%s]}\n", new->offset, new->symbol, new->table_index);
-		// list_add_last_symtable(list, offset, type(section->sectname, array[i].n_type,
-		// 	array[i].n_value, array[i].n_sect), stringtable+array[i].n_un.n_strx);
-		list_add_order_symtable(list, new, compare_tableindex);
-		ft_printf("   {list add %32kOK%k}\n");
+		add_symtable(array[i], section, stringtable, list);
 		i++;
 	}
 }
@@ -200,7 +211,7 @@ void	handle_64(char *ptr, t_symtable **list)
 	}
 }
 
-void	nm(char *ptr)
+int		nm(char *ptr)
 {
 	unsigned int		magic_number;
 	t_symtable			*list;
@@ -208,9 +219,20 @@ void	nm(char *ptr)
 	magic_number = *(unsigned int *)ptr;
 	if (magic_number == MH_MAGIC_64)
 	{
-		ft_printf("%33k<call print_output>%k\n");
+		ft_printf("%33k<call handle_64>%k\n");
 		handle_64(ptr, &list);
-		ft_printf("%33k<end print_output>%k\n");
+		ft_printf("%33k<end handle_64>%k\n");
+	}
+	else if (magic_number == FAT_CIGAM)
+	{
+		ft_printf("%33k<magic_number = %x | FAT_CIGAM>%k\n", magic_number);
+		ft_printf("%36k~Todo: handle FAT_CIGAM binaries~%k\n", magic_number);
+		return (1);
+	}
+	else
+	{
+		ft_printf("%33k<magic_number = %x>%k\n", magic_number);
+		return (0);
 	}
 	// @TODO: gérer ici les fat binaries
 	ft_printf("%33k<call print_output>%k\n");
@@ -219,6 +241,7 @@ void	nm(char *ptr)
 	ft_printf("%33k<call free_symtable>%k\n");
 	free_symtable(&list);
 	ft_printf("%33k<end free_symtable>%k\n");
+	return (1);
 }
 
 int		main(int ac, char **av)
@@ -226,19 +249,31 @@ int		main(int ac, char **av)
 	int				fd;
 	struct stat		buf;
 	char			*ptr;
+	int				i;
 
-	if (ac != 2)
+	if (ac < 2)
+	{
+		ft_printf_fd(2, "There are %d args given to the function\n", ac);
 		return (print_error("Please give me an arg"));
-	if ((fd = open(av[1], O_RDONLY)) < 0)
-		return (file_error("Can't open ", av[1]));
-	if (fstat(fd, &buf) < 0)
-		return (file_error("Error fstat : ", av[1]));
-	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0))
-		== MAP_FAILED)
-		return (file_error("Error mmap : ", av[1]));
-	nm(ptr);
-	if (munmap(ptr, buf.st_size) < 0)
-		return (file_error("Error munmap : ", av[1]));
-	close(fd);
+	}
+	i = 1;
+	while (++i <= ac)
+	{
+		if (ac > 2)
+			ft_printf("%s:\n", av[i - 1]);
+		if ((fd = open(av[i - 1], O_RDONLY)) < 0)
+			return (file_error("No such file or directory.", av[i - 1], av[0]));
+		if (fstat(fd, &buf) < 0)
+			return (file_error("Error fstat.", av[i - 1], av[0]));
+		if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0))
+			== MAP_FAILED)
+			return (file_error("Error mmap.", av[i - 1], av[0]));
+		if (nm(ptr) == 0)
+			return (file_error("The file was not recognized as a valid object file", av[i - 1], av[0]));
+		if (munmap(ptr, buf.st_size) < 0)
+			return (file_error("Error munmap.", av[i - 1], av[0]));
+		close(fd);
+	}
+
 	return (EXIT_SUCCESS);
 }
