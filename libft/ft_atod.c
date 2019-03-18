3666975cbc15a7553fcf2215994cf05a8fc93a7e
Emilien Baudet <emilien.baudet@gmail.com>
2019-03-18 16:02:11 +0100

    no use of maths.h

--- libft/ft_atod.c
+++ libft/ft_atod.c
@@ -6,18 +6,30 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 16:39:09 by ebaudet           #+#    #+#             */
-/*   Updated: 2014/03/25 21:43:17 by ebaudet          ###   ########.fr       */
+/*   Updated: 2019/03/18 15:50:01 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <math.h>
+
+double ft_pow10(unsigned int y)
+{
+	double result;
+
+	result = 1;
+	while (y >= 1)
+	{
+		result *= 10;
+		y--;
+	}
+	return (result);
+}
 
 double		ft_atod(const char *str)
 {
-	double	value;
-	int		sign;
-	int		i;
+	double			value;
+	int				sign;
+	unsigned int	i;
 
 	value = 0;
 	sign = (*str == '-') ? -1 : 1;
@@ -36,7 +48,7 @@ double		ft_atod(const char *str)
 		i = 1;
 		while (ft_isdigit(*str))
 		{
-			value += (double)(*str - '0') / pow(10, i++);
+			value += (double)(*str - '0') / ft_pow10(i++);
 			str++;
 		}
 	}
