89cc9c574d8a4c9b11219fac788c513533de4479
Emilien Baudet <emilien.baudet@gmail.com>
2019-03-20 17:48:31 +0100

    ft_pow corrections

--- includes/math.h
+++ includes/math.h
@@ -6,14 +6,14 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/24 19:51:44 by ebaudet           #+#    #+#             */
-/*   Updated: 2019/02/16 16:42:30 by ebaudet          ###   ########.fr       */
+/*   Updated: 2019/03/20 17:47:11 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATH_H
 # define MATH_H
 
-long long int	ft_pow(int nb, int pow);
+long long int	ft_pow(int nb, unsigned int pow);
 void			ft_puthex(long long int nb, int width);
 char			*ft_gethex(long long int nb, int width);
 
