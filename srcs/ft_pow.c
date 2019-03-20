88338050e0ba4539ff9bf6ea1c58ca2c0f55595a
Emilien BAUDET <ebaudet@e3r1p9.42.fr>
2019-03-20 17:48:31 +0100

    ft_pow corrections

--- srcs/ft_pow.c
+++ srcs/ft_pow.c
@@ -6,23 +6,20 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/24 19:50:19 by ebaudet           #+#    #+#             */
-/*   Updated: 2019/01/14 22:21:31 by ebaudet          ###   ########.fr       */
+/*   Updated: 2019/03/20 17:48:00 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-long long int		ft_pow(int nb, int pow)
+long long int	ft_pow(int nb, unsigned int pow)
 {
-	int				i;
+	unsigned int	i;
 	long long int	result;
 
 	if (pow == 0)
 		return (1);
 	result = nb;
-	i = 1;
-	while (i < pow)
-	{
+	i = 0;
+	while (++i < pow)
 		result *= nb;
-		i++;
-	}
 	return (result);
 }
