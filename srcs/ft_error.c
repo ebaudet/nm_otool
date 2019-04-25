5f8f7abe95eb5eaafda59e37d6a432dd4d10f1f2
Emilien Baudet <emilien.baudet@gmail.com>
2019-04-25 19:41:52 +0200

    refactoring input nm parts

--- srcs/ft_error.c
+++ srcs/ft_error.c
@@ -6,13 +6,14 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/27 20:54:13 by ebaudet           #+#    #+#             */
-/*   Updated: 2019/02/18 17:44:27 by ebaudet          ###   ########.fr       */
+/*   Updated: 2019/04/25 19:31:38 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 #include "libftprintf.h"
+#include "nm.h"
 
 int		print_error(char *message)
 {
@@ -21,8 +22,8 @@ int		print_error(char *message)
 	return (EXIT_FAILURE);
 }
 
-int		file_error(char *message, char *file, char *function)
+int		file_error(char *message, t_nm *nm)
 {
-	ft_printf_fd(2, "%s: %s %s\n", function, file, message);
+	ft_printf_fd(2, "%s: %s %s\n", nm->command, nm->file, message);
 	return (EXIT_FAILURE);
 }
