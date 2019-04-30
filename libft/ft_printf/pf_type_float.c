3c02212250151a62bfe87f8240da401e17d4fc9a
Emilien Baudet <emilien.baudet@gmail.com>
2019-04-30 16:30:15 +0200

    norme libprintf

--- libft/ft_printf/pf_type_float.c
+++ libft/ft_printf/pf_type_float.c
@@ -6,7 +6,7 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 18:58:11 by ebaudet           #+#    #+#             */
-/*   Updated: 2019/04/29 18:59:59 by ebaudet          ###   ########.fr       */
+/*   Updated: 2019/04/30 16:27:54 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,17 @@ void	type_f(t_ftprintf *t, t_params *params)
 	char		*str;
 	long double	value;
 
+	value = (params->length == LD)
+		? va_arg(t->ap, long double)
+		: va_arg(t->ap, double);
 	if (params->length == LD)
-	{
-		value = va_arg(t->ap, long double);
 		str = (params->precision == -1)
 			? ft_ldtoa(value, 6)
 			: ft_ldtoa(value, params->precision);
-	}
 	else
-	{
-		value = va_arg(t->ap, double);
 		str = (params->precision == -1)
 			? ft_dtoa(value, 6)
 			: ft_dtoa(value, params->precision);
-	}
 	if (check_flag(params, PLUS) && (value > 0))
 		add_to_buff(params, "+", -1);
 	if (!check_flag(params, PLUS) && check_flag(params, SPACE) && (value > 0))
