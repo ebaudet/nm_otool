1f168c554c5883ef5bc6294065090e309f5a55a5
Emilien Baudet <emilien.baudet@gmail.com>
2019-09-26 13:41:15 +0200

    norme files

--- srcs/utils/ft_in_array.c
+++ srcs/utils/ft_in_array.c
@@ -6,7 +6,7 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 02:31:42 by ebaudet           #+#    #+#             */
-/*   Updated: 2019/05/18 02:39:27 by ebaudet          ###   ########.fr       */
+/*   Updated: 2019/09/26 13:29:30 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	ft_in_array(const unsigned int *haystack, size_t size, unsigned int needle)
 	size_t	i;
 
 	i = 0;
-	while (i < size) {
+	while (i < size)
+	{
 		if (haystack[i] == needle)
 			return (1);
 		i++;
