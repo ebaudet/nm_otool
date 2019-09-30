ad56ac5ccb1a144cfa1f96f3cc90b0199072184c
Emilien Baudet <emilien.baudet@gmail.com>
2019-09-30 19:05:49 +0200

    NM: error load commands extend past the end of file DONE

--- srcs/nm/handle_file.c
+++ srcs/nm/handle_file.c
@@ -6,7 +6,7 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 16:37:38 by ebaudet           #+#    #+#             */
-/*   Updated: 2019/09/06 15:03:35 by ebaudet          ###   ########.fr       */
+/*   Updated: 2019/09/30 19:03:44 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,18 @@
 #include "libft.h"
 #include "libftprintf.h"
 
-int		handle_file(t_nm *nm)
+static int		nm_error_handle(t_nm *nm, int code_error)
+{
+	if (code_error == ERROR_LC_PTR)
+		nm_error("truncated or malformed object (load commands ex\
+tend past the end of the file)", nm);
+	else
+		nm_error("The file was not recognized as a valid objec\
+t file\n", nm);
+	return (EXIT_FAILURE);
+}
+
+int				handle_file(t_nm *nm)
 {
 	int				fd;
 	struct stat		buf;
@@ -36,9 +47,8 @@ int		handle_file(t_nm *nm)
 		return (nm_error("Error mmap.", nm));
 	get_ptr(ptr);
 	get_size(buf.st_size);
-	if (handle_type(nm, ptr, NULL) == 0)
-		return_value = nm_error("The file was not recognized as a valid objec\
-t file\n", nm);
+	if ((return_value = handle_type(nm, ptr, NULL)) != EXIT_SUCCESS)
+		return_value = nm_error_handle(nm, return_value);
 	if (munmap(ptr, buf.st_size) < 0)
 		return_value = nm_error("Error munmap.", nm);
 	close(fd);
